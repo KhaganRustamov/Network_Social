d1fd5afa0e30881a02a9a6cebdc595f7f0e06d06
Khagan <khaqan.rustamov@gmail.com>
2024-12-08 13:00:14 +0400

    create post controller

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -10,6 +10,7 @@ const PostController = {
     }
 
     try {
+      // Create post
       const post = await prisma.post.create({
         data: {
           content,
@@ -23,8 +24,10 @@ const PostController = {
       res.status(500).json({ error: "Internal server error" });
     }
   },
+
   getAllPosts: async (req, res) => {
     const userId = req.user.userId;
+    // Get all posts
     try {
       const posts = await prisma.post.findMany({
         include: {
@@ -37,6 +40,7 @@ const PostController = {
         },
       });
 
+      // Check liked posts
       const postWithLikeInfo = posts.map((post) => ({
         ...post,
         likedByUser: post.likes.some((like) => like.userId === userId),
@@ -48,11 +52,62 @@ const PostController = {
       res.status(500).json({ error: "Internal server error" });
     }
   },
+
   getPostById: async (req, res) => {
-    res.send("getPostById");
+    const { id } = req.params;
+    const userId = req.user.userId;
+
+    try {
+      // Get post by id
+      const post = await prisma.post.findUnique({
+        where: { id },
+        include: {
+          likes: true,
+          author: true,
+          comments: {
+            include: {
+              user: true,
+            },
+          },
+        },
+      });
+
+      if (!post) {
+        return res.status(404).json({ error: "Post is not found" });
+      }
+      // Check like post
+      const postWithLikeInfo = {
+        ...post,
+        likedByUser: post.likes.some((like) => like.userId === userId),
+      };
+
+      res.json(postWithLikeInfo);
+    } catch (error) {
+      console.error("Error in get post:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   },
+
   deletePost: async (req, res) => {
-    res.send("deletePost");
+    const { id } = req.params;
+    const post = await prisma.post.findUnique({ where: { id } });
+
+    if (!post) {
+      return res.status(404).json({ error: "Post is not found" });
+    }
+
+    // Checking that the user deleted own post
+    if (post.authorId !== req.user.userId) {
+      return res.status(403).json({ error: "Not access" });
+    }
+
+    try {
+      await prisma.post.delete({ where: { id } });
+      res.json("Post deleted successfully");
+    } catch (error) {
+      console.error("Error in delete post:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   },
 };
 
